7e207c0bc5c5444195b59fa1dd0cea4e6ac90520
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-09-24 16:31:46 +0300

    Setting up

---
+++ Controllers/DiffController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_diff.Controllers
+{
+  [Route("v1/[controller]")]
+  [ApiController]
+  public class DiffController : ControllerBase
+  {
+    // POST v1/diff/{id}/left
+    [HttpPost("{id}/left")]
+    public ActionResult<string> DiffLeft(int id, [FromBody] string value)
+    {
+      return "ok";
+    }
+
+    // POST v1/diff/{id}/right
+    [HttpPost("{id}/right")]
+    public ActionResult<string> DiffRight(int id, [FromBody] string value)
+    {
+      return "ok";
+    }
+
+    // GET v1/diff/{id}
+    [HttpGet("{id}")]
+    public ActionResult<string> DiffResult(int id)
+    {
+      return "diff result";
+    }
+
+  }
+}
